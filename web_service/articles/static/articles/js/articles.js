b10d2a7f770a999371ff6dafdb91868739a71996
stacey_smolnaya <a.smolnaja@ya.ru>
2021-10-15 23:34:10 +0300

    frontend

--- web_service/articles/static/articles/js/articles.js
+++ web_service/articles/static/articles/js/articles.js
@@ -1,76 +1,75 @@
 $(function () {
 
-  /* Functions */
+    /* Functions */
 
-  var loadForm = function () {
-    var btn = $(this);
-    $.ajax({
-      url: btn.attr("data-url"),
-      type: 'get',
-      dataType: 'json',
-      beforeSend: function () {
-        $("#modal-article .modal-content").html("");
-        $("#modal-article").modal("show");
-      },
-      success: function (data) {
-        $("#modal-article .modal-content").html(data.html_form);
-      }
-    });
-  };
+    var loadForm = function () {
+        var btn = $(this);
+        $.ajax({
+            url: btn.attr("data-url"),
+            type: 'get',
+            dataType: 'json',
+            beforeSend: function () {
+                $("#modal-article .modal-content").html("");
+                $("#modal-article").modal("show");
+            },
+            success: function (data) {
+                $("#modal-article .modal-content").html(data.html_form);
+            }
+        });
+    };
 
-  var viewForm = function () {
-    var btn = $(this);
-    $.ajax({
-      url: btn.attr("data-url"),
-      type: 'get',
-      dataType: 'json',
-      beforeSend: function () {
-        $("#modal-article .modal-content").html("");
-        $("#modal-article").modal("show");
-      },
-      success: function (data) {
-        $("#modal-article .modal-content").html(data.html_form);
-      }
-    });
-  };
+    var viewForm = function () {
+        var btn = $(this);
+        $.ajax({
+            url: btn.attr("data-url"),
+            type: 'get',
+            dataType: 'json',
+            beforeSend: function () {
+                $("#modal-article .modal-content").html("");
+                $("#modal-article").modal("show");
+            },
+            success: function (data) {
+                $("#modal-article .modal-content").html(data.html_form);
+            }
+        });
+    };
 
-  var saveForm = function () {
-    var form = $(this);
-    $.ajax({
-      url: form.attr("action"),
-      data: form.serialize(),
-      type: form.attr("method"),
-      dataType: 'json',
-      success: function (data) {
-        if (data.form_is_valid) {
-          $("#article-table tbody").html(data.html_article_list);
-          $("#modal-article").modal("hide");
-        }
-        else {
-          $("#modal-article .modal-content").html(data.html_form);
-        }
-      }
-    });
-    return false;
-  };
+    var saveForm = function () {
+        var form = $(this);
+        $.ajax({
+            url: form.attr("action"),
+            data: form.serialize(),
+            type: form.attr("method"),
+            dataType: 'json',
+            success: function (data) {
+                if (data.form_is_valid) {
+                    $("#article-table tbody").html(data.html_article_list);
+                    $("#modal-article").modal("hide");
+                } else {
+                    $("#modal-article .modal-content").html(data.html_form);
+                }
+            }
+        });
+        return false;
+    };
 
 
-  /* Binding */
+    /* Binding */
 
-  // Create article
-  $(".js-create-article").click(loadForm);
-  $("#modal-article").on("submit", ".js-article-create-form", saveForm);
+    // Create article
+    $(".js-create-article").click(loadForm);
+    $("#modal-article").on("submit", ".js-article-create-form", saveForm);
 
-  // Open article
-  $("#article-table").on("click", ".js-open-article", viewForm);
-  $("#modal-article").on("submit", ".js-article-open-form", saveForm);
+    // Open article
+    $("#article-table").on("click", ".js-open-article", viewForm);
+    $("#modal-article").on("submit", ".js-article-open-form", saveForm);
 
-  // Update article
-  $("#article-table").on("click", ".js-update-article", loadForm);
-  $("#modal-article").on("submit", ".js-article-update-form", saveForm);
+    // Update article
+    $("#article-table").on("click", ".js-update-article", loadForm);
+    $("#modal-article").on("submit", ".js-article-update-form", saveForm);
 
-  // Delete article
-  $("#article-table").on("click", ".js-delete-article", loadForm);
-  $("#modal-article").on("submit", ".js-article-delete-form", saveForm);
+    // Delete article
+    $("#article-table").on("click", ".js-delete-article", loadForm);
+    $("#modal-article").on("submit", ".js-article-delete-form", saveForm);
 
 });
